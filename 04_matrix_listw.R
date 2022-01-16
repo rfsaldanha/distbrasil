c9f4c2d2f80ce3d83c0c6bb29995d158f241929d
Raphael Saldanha <rfsaldanha@gmail.com>
2022-01-16 19:41:06 -0300

    export matrix

--- 04_matrix_listw.R
+++ 04_matrix_listw.R
@@ -1,26 +1,66 @@
 library(tidyverse)
+library(spdep)
+library(geobr)
 
-dist_brasil <- readRDS(file = "dist_brasil.rds")
+# Leitura do data frame de distâncias
+dist_brasil <- readRDS(file = "dist_brasil_full.rds")
 
+# Remove duplicidades e pares de municípios
+# onde não foi possível determinar a distância rodoviária
 dist_brasil2 <- dist_brasil %>% 
   distinct(orig, dest, .keep_all = TRUE) %>%
   na.omit()
 
+# Lista frequencia de pares para cada município
 freqs <- dist_brasil2 %>%
   group_by(orig) %>%
   summarise(freq = n()) %>%
   ungroup() %>%
   filter(freq < max(freq))
 
+# Remove municípios onde não foi estabelecer distâncias
+# para todos os pares
 dist_brasil3 <- dist_brasil2 %>%
   filter(!(orig %in% freqs$orig))
 
+# Transforma o data frame em uma matriz com as distâncias
 m_dist <- matrix(data = dist_brasil3$dist, 
                  ncol = 5553, byrow = TRUE,
                  dimnames = list(unique(dist_brasil3$orig), unique(dist_brasil3$dest)))
 
-m_listw <- spdep::mat2listw(x = m_dist, row.names = unique(dist_brasil3$orig))
 
-m_sn <- spdep::listw2sn(listw = m_listw)
+# Converte a matriz um objeto do tipo listw
+dist_listw <- mat2listw(x = m_dist, row.names = unique(dist_brasil3$orig))
+saveRDS(object = dist_listw, file = "dist_listw.rds")
 
-spdep::write.sn2gwt(sn = m_sn, file = "wdat.gwt")
+# listw para sn
+dist_sn <- spdep::listw2sn(listw = dist_listw)
+
+# sn para arquivo GWT do Geoda
+write.sn2gwt(sn = dist_sn, file = "dist.gwt", ind = "code_mn", useInd = TRUE)
+
+# Transforma o data frame em uma matriz com o tempo
+m_temp <- matrix(data = dist_brasil3$dur, 
+                 ncol = 5553, byrow = TRUE,
+                 dimnames = list(unique(dist_brasil3$orig), unique(dist_brasil3$dest)))
+
+
+# Converte a matriz um objeto do tipo listw
+temp_listw <- mat2listw(x = m_temp, row.names = unique(dist_brasil3$orig))
+saveRDS(object = temp_listw, file = "temp_listw.rds")
+
+# listw para sn
+temp_sn <- spdep::listw2sn(listw = temp_listw)
+
+# sn para arquivo GWT do Geoda
+write.sn2gwt(sn = temp_sn, file = "temp.gwt", ind = "code_mn", useInd = TRUE)
+
+
+# Exporta shapefile compatível com a matriz
+mun_map <- read_municipality() %>%
+  filter(code_muni %in% unique(dist_brasil3$orig)) %>%
+  mutate(code_muni = as.integer(code_muni))
+
+st_write(mun_map, "mun_shapefile/brasil_mun.shp", driver = "ESRI Shapefile")
+
+
